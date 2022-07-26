def72c0abb68abf290502a26912364fee4fa287c
sandufurdui <furdui.alexandru@isa.utm.md>
2022-07-26 16:16:32 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Login from "./pages/login_page";
-// import Dashboard from "./pages/dash_page";
-// import Main from "./pages/main_page"
-// import About from "./pages/about_page"
-// import Register from "./pages/signup_page"
-// import Reset from "./pages/reset_page"
+import Login from "./pages/login_page";
+import Dashboard from "./pages/dash_page";
+import Main from "./pages/main_page"
+import About from "./pages/about_page"
+import Register from "./pages/signup_page"
+import Reset from "./pages/reset_page"
 
 function App() {
   return (
     <>
-
-    hello mf
-      {/* <Router>
+      <Router>
         <Routes>
         <Route path="/" element={<Main />}></Route>
           <Route path="/login" element={<Login />}></Route>
@@ -20,7 +18,7 @@ function App() {
           <Route path="/about" element={<About />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
         </Routes>
-        </Router> */}
+        </Router>
     </>
   );
 }
